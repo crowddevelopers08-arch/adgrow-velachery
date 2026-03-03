aa385334cf944106cf24daca07651ad9a9e4922e
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-03-03 12:58:50 +0530

    skin velachery tag attach

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,24 @@ import CTABanner from "@/components/review";
 import BeautyServicesSection from "@/components/skinpopup";
 import ImageGridCarousel from "@/components/video";
 import ClinicVideosResponsiveGrid from "@/components/videosection";
-
+import Script from "next/script";
 
 export default function HomePage() {
   return (
     <main className="bg-background">
+      <Script
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vpuoes0psh");
+          `
+        }}
+      />
       <GrohairTopBar />
       <VelacheryBeautyBanner />
       <BeautyServicesSection />
@@ -26,7 +39,6 @@ export default function HomePage() {
       <FAQSection />
       <ContactSection />
       <MobileActionBar />
-
     </main>
   );
 }
